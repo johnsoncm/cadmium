1223683dc39a3b21b5a3b84f9169c3dbec588ea8
Catherine Smith <catsmith24601@gmail.com>
2021-06-05 12:54:51 -0400

    accidentally made changes to profile for timepicker on main instead of branhc

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,36 +1,36 @@
 const addEventHandler = async (event) => {
   event.preventDefault();
-console.log("add event handler is running")
-  const title = document.querySelector('#event-name').value.trim();
+
+  const name = document.querySelector('#event-name').value.trim();
   const date = document.querySelector('#datepicker').value.trim();
-  const time = document.querySelector('#time').value.trim();
-  const locationName = document.querySelector('#venue').value.trim();
-  const locationAddress = document.querySelector('#venue-address').value.trim();
+  const time = document.querySelector('#timepicker').value.trim();
+  const venue = document.querySelector('#venue').value.trim();
+  const venueAddress = document.querySelector('#venue-address').value.trim();
   const description = document.querySelector('#event-description').value.trim();
   const img = document.querySelector('#img').value.trim();
 
 // This isn't quite right - need to account for default img if user does not have one
-  if (title && date && time && locationName && locationAddress && description && img) {
-    const response = await fetch('/api/events', {
+  if (name && date && time && venue && venueAddress && description && img) {
+    const response = await fetch(`/api/events`, {
       method: 'POST',
       body: JSON.stringify({ 
-        title, 
+        name, 
         date,
         time,
-        locationName,
-        locationAddress,
+        venue,
+        venueAddress,
         description,
         img
       }),
-          headers: {
+      headers: {
         'Content-Type': 'application/json',
       },
     });
-console.log('response' , response)
+
     if (response.ok) {
       document.location.replace('/homepage');
     } else {
-      alert('Failed to create profile');
+      alert('Error: Failed to create new event');
     }
   }
 };
@@ -47,6 +47,7 @@ $(function () {
 $(function () {
   $('#timepicker').timepicker({
     timeFormat: 'h:mm p',
+    interval: 15
   });
 });
 
@@ -60,17 +61,16 @@ const delButtonHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/homepage');
     } else {
-      alert('Failed to delete profile');
+      alert('Failed to delete event');
     }
   }
 };
 
 
-
 document
-  .querySelector('#new-event-form')
+  .querySelector('.new-event-form')
   .addEventListener('submit', addEventHandler);
 
 document
