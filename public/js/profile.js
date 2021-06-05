9af9718b7de5cad5e2ab78231c180800a7ff1305
Catherine Smith <catsmith24601@gmail.com>
2021-06-05 13:29:36 -0400

    fixing maybe

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,24 +1,24 @@
 const addEventHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#event-name').value.trim();
+  const title = document.querySelector('#event-name').value.trim();
   const date = document.querySelector('#datepicker').value.trim();
   const time = document.querySelector('#timepicker').value.trim();
-  const venue = document.querySelector('#venue').value.trim();
-  const venueAddress = document.querySelector('#venue-address').value.trim();
+  const locationName = document.querySelector('#venue').value.trim();
+  const locationAddress = document.querySelector('#venue-address').value.trim();
   const description = document.querySelector('#event-description').value.trim();
   const img = document.querySelector('#img').value.trim();
 
 // This isn't quite right - need to account for default img if user does not have one
-  if (name && date && time && venue && venueAddress && description && img) {
-    const response = await fetch(`/api/events`, {
+  if (title && date && time && locationName && locationAddress && description && img) {
+    const response = await fetch('/api/newevent', {
       method: 'POST',
       body: JSON.stringify({ 
-        name, 
+        title, 
         date,
         time,
-        venue,
-        venueAddress,
+        locationName,
+        locationAddress,
         description,
         img
       }),
@@ -28,7 +28,7 @@ const addEventHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/homepage');
+      document.location.replace('/newevent');
     } else {
       alert('Error: Failed to create new event');
     }
@@ -56,12 +56,13 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/profile/${id}`, {
+    const response = await fetch(`/api/newevent/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
-      document.location.replace('/homepage');
+      // document.location.replace('/homepage');
+      document.location.replace('/newevent');
     } else {
       alert('Failed to delete event');
     }
